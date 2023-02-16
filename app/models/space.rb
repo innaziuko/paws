0c1fc9fe6589d9c5e80bf26010aa6b113a1344e9
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2023-02-16 20:15:45 +0100

    Space Model

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,9 @@
 class Space < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :contact, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, numericality: true
 end
